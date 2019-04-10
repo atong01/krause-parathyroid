6e2cb60deb07c1924abdfa0e6a9a0e8305509a71
Alex Tong <alexanderytong@gmail.com>
2019-04-10 18:59:39 -0400

    Update magic

--- generate_data.py
+++ generate_data.py
@@ -6,61 +6,67 @@ import magic
 import pickle
 import graphtools
 
+
+def get_common_genes(sample_list):
+    common_set = set(sample_list[0].columns.values.tolist())
+    for s in sample_list[1:]:
+        common_set = common_set.intersection(set(s.columns.values.tolist()))
+    return list(common_set)
+
 data_name = "PPA"
 
-paray9 = scprep.io.load_10X_HDF5(os.path.join(
-    os.path.abspath(os.sep),
-    "data", "lab", "DataSets",
-    "Krause_2018_primary_parathyroid_adenoma",
-    "ParaY9_HHT_cellranger",
-    "filtered_gene_bc_matrices_h5.h5",  # raw_gene_bc_matrices
-), gene_labels='both', allow_duplicates=True)
-paray9 = scprep.filter.remove_rare_genes(paray9, min_cells=3)
+SAMPLE_NAMES = ['Y7', 'Y9', 'Y11', 'Y13']
+SEED = 42
+samples = []
+s = scprep.io.load_10X_HDF5(os.path.join(
+        os.path.abspath(os.sep),
+        "data", "lab", "DataSets",
+        "Krause_2018_primary_parathyroid_adenoma",
+        "paray7_HHT_cellranger",
+        "filtered_gene_bc_matrices_h5.h5",
+    ), gene_labels='both', allow_duplicates=True)
+samples.append(s)
+s = scprep.io.load_10X_HDF5(os.path.join(
+        os.path.abspath(os.sep),
+        "data", "lab", "DataSets",
+        "Krause_2018_primary_parathyroid_adenoma",
+        "ParaY9_HHT_cellranger",
+        "filtered_gene_bc_matrices_h5.h5",
+    ), gene_labels='both', allow_duplicates=True)
+samples.append(s)
+for sample in SAMPLE_NAMES[2:]:
+    s = scprep.io.load_10X(os.path.join(
+        os.path.abspath(os.sep),
+        "data", "lab", "DataSets",
+        "Krause_2019_primary_parathyroid_adenoma",
+        "%s_HHT_cellranger" % sample,
+        "filtered_feature_bc_matrix",
+    ), gene_labels='both', allow_duplicates=True)
+    # s = scprep.filter.remove_rare_genes(s, min_cells=3)
+    samples.append(s)
+    
+common_genes = get_common_genes(samples)
+samples = [s[common_genes] for s in samples]
+samples_norm = [scprep.normalize.library_size_normalize(s) for s in samples]
+samples_sqrt_norm = [scprep.transform.sqrt(s) for s in samples]
+data, sample_labels = scprep.utils.combine_batches(samples_sqrt_norm, SAMPLE_NAMES)
+data.astype('float32')
 
-paray9_norm = scprep.normalize.library_size_normalize(paray9)
-paray9_norm = scprep.transform.sqrt(paray9_norm)
-
-paray7 = scprep.io.load_10X(os.path.join(
-    os.path.abspath(os.sep),
-    "data", "lab", "DataSets",
-    "Krause_2018_primary_parathyroid_adenoma",
-    "paray7_HHT_cellranger",
-    "filtered_gene_bc_matrices",  # raw_gene_bc_matrices
-    "GRCh38"), gene_labels='both')
-paray7 = scprep.filter.remove_rare_genes(paray7, min_cells=3)
-
-paray7_norm = scprep.normalize.library_size_normalize(
-    paray7, rescale=paray9_norm.iloc[0].sum())
-paray7_norm = scprep.transform.sqrt(paray7_norm)
-
-common_genes = set(paray7_norm.columns.values.tolist())
-common_genes = common_genes.intersection(paray9_norm.columns.values.tolist())
-data, sample_labels = scprep.utils.combine_batches(
-    [paray7_norm[list(common_genes)], paray9_norm[list(common_genes)]],
-    ['Y7', 'Y9'])
-
-mnn_graph = graphtools.Graph(data, sample_idx=sample_labels, n_pca=100, knn=5,
-                             random_state=42,
-                             decay=15, kernel_symm='theta', theta=0.99)
-pth_idx = (data[scprep.utils.get_gene_set(
-    data, starts_with="PTH ").values] >= 11).values.flatten()
-
-
-ph = phate.PHATE(n_components=2,
-                 random_state=42,
-                 knn_dist='precomputed')
-phate_data = ph.fit_transform(mnn_graph.kernel)
-np.save("{}Phate2d.npy".format(data_name), phate_data[pth_idx])
-
-ph.set_params(n_components=3)
-phate3_data = ph.transform()
-np.save("{}Phate3d.npy".format(data_name), phate3_data[pth_idx])
-
-mnn_graph = graphtools.Graph(data.iloc[pth_idx],
-                             sample_idx=sample_labels[pth_idx],
+mnn_graph = graphtools.Graph(data,
+                             sample_idx=sample_labels,
                              n_pca=100, knn=5,
                              random_state=42,
                              decay=15, kernel_symm='theta', theta=0.99)
+
+phate_op = phate.PHATE(n_components=3,
+                       random_state=42,
+                       knn_dist='precomputed', n_jobs=36)
+mnn_data_phate_3d = phate_op.fit_transform(mnn_graph.K)
+phate_op.set_params(n_components=2)
+mnn_data_phate_2d = phate_op.fit_transform(mnn_graph.K)
+np.save("{}Phate2d.npy".format(data_name), mnn_data_phate_2d)
+np.save("{}Phate3d.npy".format(data_name), mnn_data_phate_3d)
+
 mg = magic.MAGIC(
     random_state=42,
     a=mnn_graph.decay, k=mnn_graph.knn - 1, n_pca=mnn_graph.n_pca)
